55a1403861d8268f428bfcef900ad11721af0ef5
jonad sebastien <105843973+jonseb974@users.noreply.github.com>
2022-11-28 22:09:01 +0100

    test_hello_route

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -2,9 +2,14 @@
 from flask import Flask
 
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 
 
-@app.route("/")
+@app.route("/", strict_slashes = False)
 def hello_HBNB():
+    """Display a hello message"""
+
     return "<p>Hello HBNB!</p>"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
