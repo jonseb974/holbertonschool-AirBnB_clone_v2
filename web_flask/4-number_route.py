10f098516fb53ea0fe64171f50b34909452b5027
jonad sebastien <105843973+jonseb974@users.noreply.github.com>
2022-11-29 17:56:54 +0100

    create_file4_number_route

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -52,8 +52,8 @@ def number(n):
     """Display number"""
 
     n = int(n)
-    return "n is " + str(n)
+    return "n is " + int(n)
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
