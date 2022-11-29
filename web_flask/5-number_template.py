2ea01ae6f5139e6d4f7e731a88aa52850a3399c6
jonad sebastien <105843973+jonseb974@users.noreply.github.com>
2022-11-29 20:52:00 +0100

    pycodestyle update

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -61,12 +61,14 @@ def python(text="is cool"):
 
     return "Python {}".format(text)
 
+
 @app.route("/number/<int:n>", strict_slashes=False)
 def number(n):
     """Display n is a  number
     only if n is an integer
     """
     return "{} is a number".format(n)
+
 
 @app.route("/number_template/<int:n>", strict_slashes=False)
 def number_template(n):
@@ -79,4 +81,3 @@ def number_template(n):
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
-
