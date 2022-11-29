586fe8d1a408613e66b80472ea6a858a365d867f
jonad sebastien <105843973+jonseb974@users.noreply.github.com>
2022-11-29 21:51:49 +0100

    file6-number_odd_or_even update

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -3,21 +3,21 @@
 
 Web application must be listening on 0.0.0.0, port 5000
 Routes:
-    /: display “Hello HBNB!”
-    /hbnb: display “HBNB”
+/: display “Hello HBNB!”
+/hbnb: display “HBNB”
 
-    /c/<text>: display “C ”, followed by the value
-    of the text variable (replace underscore _ symbols with a space )
-    /python/<text>: display “Python ”,
-    followed by the value of
-    the text variable (replace underscore _ symbols with a space )
+/c/<text>: display “C ”, followed by the value
+of the text variable (replace underscore _ symbols with a space )
+/python/<text>: display “Python ”,
+followed by the value of
+the text variable (replace underscore _ symbols with a space )
 
 The default value of text is “is cool”
-    /number/<n>: display “n is a number” only if n is an integer
-    /number_template/<n>: display a HTML page only if n is an integer:
+/number/<n>: display “n is a number” only if n is an integer
+/number_template/<n>: display a HTML page only if n is an integer:
 
 H1 tag: “Number: n” inside the tag BODY
-    /number_odd_or_even/<n>: display a HTML page only if n is an integer:
+/number_odd_or_even/<n>: display a HTML page only if n is an integer:
 H1 tag: “Number: n is even|odd” inside the tag BODY
 You must use the option strict_slashes=False in your route definition
 """
@@ -51,6 +51,7 @@ def c(text):
     text = text.replace("_", " ")
 
     return "C {}" .format(text)
+
 
 @app.route("/python", strict_slashes=False)
 @app.route("/python/<text>", strict_slashes=False)
