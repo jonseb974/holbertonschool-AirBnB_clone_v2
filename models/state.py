3638b2a7983940b0b8fff05156a7a43a5e567d89
jonad sebastien <105843973+jonseb974@users.noreply.github.com>
2022-11-30 15:21:06 +0100

    add sate update

--- models/state.py
+++ models/state.py
@@ -1,11 +1,28 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel, Base
+import models
+from os import getenv
+from models.base_model import BaseModel
+from models.base_model import Base
+from models.city import City
 from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.orm import relationship
+from sqlalchemy.orm import relationship, backref
+#from sqlalchemy.orm import relationship
 
 
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state", cascade="delete")
+
+
+if getenv("HBNB_TYPE_STORAGE") != "db":
+    @property
+    def cities(self):
+        """Get a list of all related cities objects"""
+        city_list = []
+        for city in list(models.storage.all(City).values()):
+            if city.state_id == self.id:
+                city_list.append(city)
+        return city_list
