45d7d9338ba90ee58d47a1234a06840b2b2c2a15
jonad sebastien <105843973+jonseb974@users.noreply.github.com>
2022-12-01 17:21:35 +0100

    update to def all

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,20 +1,6 @@
 #!/usr/bin/python3
 """This module defines a class to manage file storage for hbnb clone"""
 import json
-from models.base_model import BaseModel
-from models.user import User
-from models.place import Place
-from models.state import State
-from models.city import City
-from models.amenity import Amenity
-from models.review import Review
-
-classes = {
-                    'BaseModel': BaseModel, 'User': User, 'Place': Place,
-                    'State': State, 'City': City, 'Amenity': Amenity,
-                    'Review': Review
-                  }
-
 
 class FileStorage:
     """This class manages storage of hbnb models in JSON format"""
@@ -47,6 +33,19 @@ class FileStorage:
 
     def reload(self):
         """Reloads storage dictionary from file"""
+        from models.base_model import BaseModel
+        from models.user import User
+        from models.place import Place
+        from models.state import State
+        from models.city import City
+        from models.amenity import Amenity
+        from models.review import Review
+
+        classes = {
+            'BaseModel': BaseModel, 'User': User, 'Place': Place,
+            'State': State, 'City': City, 'Amenity': Amenity,
+            'Review': Review
+            }
         try:
             temp = {}
             with open(FileStorage.__file_path, 'r') as f:
